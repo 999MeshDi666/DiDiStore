d181fb1ffe20a96176d67e7758b2029d50985eec
999MeshDi666 <havkpzz@gmail.com>
2022-04-18 21:56:11 +0600

    little changes js, responsive

--- DiDiStoreApp/static/DiDiStoreApp/js/animation.js
+++ DiDiStoreApp/static/DiDiStoreApp/js/animation.js
@@ -1,6 +1,3 @@
-let bdy = document.body;
-
-
 let search_line = document.querySelector('.search_line');
 let search_icon = document.getElementById('search_icon');
 let search_form = document.querySelector('.search_form');
@@ -10,45 +7,21 @@ search_icon.addEventListener('click', ()=>{
 });
 
 
-const showHideWindows = (show_prop, hide_prop, classList_prop, toggler_prop)=>{
-
-    show_prop.addEventListener('click', ()=>{
-        classList_prop.classList.toggle(toggler_prop);
-        bdy.style.overflowY = 'hidden';
-        
-    });
-    hide_prop.addEventListener('click', ()=>{
-        classList_prop.classList.toggle(toggler_prop);
-        bdy.style.overflowY = 'auto';
-    });
-}
-
-let category_window = document.querySelector('.category_window');
-let hide_categories = document.getElementById('hide_categories');
-let show_categories = document.getElementById('show_categories');
-showHideWindows(show_categories,hide_categories,category_window,'show_window');
-
-let registration_window = document.querySelector('.registration_window');
-let hide_registrations = document.getElementById('hide_registrations');
-let show_registrations = document.getElementById('show_registrations');
-showHideWindows(show_registrations,hide_registrations,registration_window,'show_window');
-
-let overlay = document.querySelector('.header__overlay');
-let hide_overlay = document.getElementById('hide_overlay');
-let show_overlay = document.getElementById('show_overlay');
-showHideWindows(show_overlay, hide_overlay,overlay,'show_window');
-
-// document.addEventListener('click', (e)=>{
-//     let id = e.target.dataset.toggleId;
-//     if(!id) return;
-//     let background = document.getElementById(id);
-//     background.hidden = !background.hidden;
-// })
+document.addEventListener('click', (e)=>{
+    let id = e.target.dataset.toggleId;
+    if(id == "categories" || id == "registrations"){
+        let overlay = document.getElementById('overlay');
+        overlay.hidden = !overlay.hidden;
+    }
+    let background = document.getElementById(id);
+    background.hidden = !background.hidden; 
+   
+})
   
 
 let sign_up = document.querySelector('.sign_up');
 let sign_in = document.querySelector('.sign_in');
-
+let registration_window = document.querySelector('.registration_window');
 let sign_up_btn = document.getElementById('#sign_up').addEventListener('click',()=>{
     registration_window.classList.remove('registration_window_sign_in')
     sign_in.style.display = 'none';
